775791113a4702e08379a423c7e16b3eec069754
polandoDOMINO5nihon <52156158+polandoDOMINO5nihon@users.noreply.github.com>
2020-05-06 12:17:48 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 #include "src/Point.h"
 #include "src/primitives.h"
 #include "src/Plate.h"
+#include "src/Object.h"
 
 using namespace std;
 
-int main()
+Object createCube()
 {
-	initGraphics(1920, 1040);
+	Object cube;
+
 	Point3D p1(-50, 50, 50);
 	Point3D p2(50, 50, 50);
 	Point3D p3(-50, -50, 50);
@@ -17,42 +19,55 @@ int main()
 	Point3D p7(-50, -50, -50);
 	Point3D p8(50, -50, -50);
 
-	Point3D rotation(0, 0.6, 0);
-	Point3D position(0, 0, -100);
-
-	cout << "TC3L Test" << endl;
 	Plate plate1(p1, p2, p3, p4);
 	Plate plate2(p5, p6, p7, p8);
 	Plate plate3(p5, p6, p1, p2);
 	Plate plate4(p7, p8, p3, p4);
 	Plate plate5(p5, p1, p7, p3);
 	Plate plate6(p2, p6, p4, p8);
-	plate1.setCameraRotation(rotation);
-	plate1.setCameraPosition(position);
-	plate2.setCameraRotation(rotation);
-	plate2.setCameraPosition(position);
-	plate3.setCameraRotation(rotation);
-	plate3.setCameraPosition(position);
-	plate4.setCameraRotation(rotation);
-	plate4.setCameraPosition(position);
-	plate5.setCameraRotation(rotation);
-	plate5.setCameraPosition(position);
-	plate6.setCameraRotation(rotation);
-	plate6.setCameraPosition(position);
 
-	plate1.render();
-	plate2.render();
-	plate3.render();
-	plate4.render();
-	plate5.render();
-	plate6.render();
+	cube.addPlate(plate1);
+	cube.addPlate(plate2);
+	cube.addPlate(plate3);
+	cube.addPlate(plate4);
+	cube.addPlate(plate5);
+	cube.addPlate(plate6);
 
-	plate1.display();
-	plate2.display();
-	plate3.display();
-	plate4.display();
-	plate5.display();
-	plate6.display();
+	return cube;
+}
+
+int main()
+{
+	initGraphics(1920, 1040);
+
+	Point3D rotation(0.2, 0.5, 0); //cam rotation
+	Point3D camPosition(0, 0, 400); //cam position in world
+
+	Point3D position1(0, 0, -800);
+	Point3D position2(100, 0, -500);
+	Point3D position3(200, 0, -500);
+	Point3D position4(300, 0, -500);
+	Point3D position5(300, -100, -500);
+	Point3D position6(0, -100, -800);
+
+	cout << "TC3L Test" << endl;
+
+	Object cube[16];
+
+	for(int i = 0; i < 16; i++)
+	{
+		cube[i] = createCube();
+		cube[i].setCameraRotation(rotation);
+		cube[i].setCameraPosition(camPosition);
+	}
+
+	cube[0].setPosition(position1);
+	cube[1].setPosition(position2);
+	cube[2].setPosition(position3);
+	cube[3].setPosition(position4);
+	cube[4].setPosition(position5);
+	cube[5].setPosition(position6);
+
 	XSelectInput(di, wi, KeyPressMask | KeyReleaseMask | ExposureMask);
 	XEvent ev;
 	XMotionEvent myszelel;
@@ -66,19 +81,12 @@ int main()
 
 		if (ev.type == Expose)
 		{
-			plate1.render();
-			plate2.render();
-			plate3.render();
-			plate4.render();
-			plate5.render();
-			plate6.render();
-
-			plate1.display();
-			plate2.display();
-			plate3.display();
-			plate4.display();
-			plate5.display();
-			plate6.display();
+			cube[0].render();
+			cube[1].render();
+			cube[2].render();
+			cube[3].render();
+			cube[4].render();
+			cube[5].render();
 		}
 	}
 	return 0;
