9ff42737b0caa4ef0a41e4d64d7d3b6cf9369440
polandoDOMINO5nihon <52156158+polandoDOMINO5nihon@users.noreply.github.com>
2020-05-07 19:07:00 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@
 
 using namespace std;
 
-/*Object createCube()
+Object createCube(Point3D *camRotation, Point3D *camPosition, Point3D rotation, Point3D position)
 {
-	Object cube;
+	Object cube(camRotation, camPosition);
 
 	Point3D p1(-50, 50, 50);
 	Point3D p2(50, 50, 50);
@@ -34,8 +34,11 @@ using namespace std;
 	cube.addPlate(plate5);
 	cube.addPlate(plate6);
 
+	cube.setRotation(rotation);
+	cube.setPosition(position);
+
 	return cube;
-}*/
+}
 
 
 
@@ -50,65 +53,18 @@ int main()
 	Point3D position2(0, 0, -600);
 	Point3D position3(0, -100, -600);
 	Point3D position4(100, -100, -600);
+	Point3D position5(100, -100, -1000);
 
-	Object cube(&camRotation, &camPosition);
-	Object cube2(&camRotation, &camPosition);
-	Object cube3(&camRotation, &camPosition);
-	Object cube4(&camRotation, &camPosition);
+	Point3D rotation0(0, 0, 0);
 
-	Point3D p1(-50, 50, 50);
-	Point3D p2(50, 50, 50);
-	Point3D p3(-50, -50, 50);
-	Point3D p4(50, -50, 50);
-	Point3D p5(-50, 50, -50);
-	Point3D p6(50, 50, -50);
-	Point3D p7(-50, -50, -50);
-	Point3D p8(50, -50, -50);
+	Object cube = createCube(&camRotation, &camPosition, rotation0, position1);
+	Object cube2 = createCube(&camRotation, &camPosition, rotation0, position2);
+	Object cube3 = createCube(&camRotation, &camPosition, rotation0, position3);
+	Object cube4 = createCube(&camRotation, &camPosition, rotation0, position4);
+	Object cube5 = createCube(&camRotation, &camPosition, rotation0, position5);
 
-	Plate plate1(p1, p2, p3, p4);
-	Plate plate2(p5, p6, p7, p8);
-	Plate plate3(p5, p6, p1, p2);
-	Plate plate4(p7, p8, p3, p4);
-	Plate plate5(p5, p1, p7, p3);
-	Plate plate6(p2, p6, p4, p8);
-
-	cube.addPlate(plate1);
-	cube.addPlate(plate2);
-	cube.addPlate(plate3);
-	cube.addPlate(plate4);
-	cube.addPlate(plate5);
-	cube.addPlate(plate6);
-
-	cube2.addPlate(plate1);
-	cube2.addPlate(plate2);
-	cube2.addPlate(plate3);
-	cube2.addPlate(plate4);
-	cube2.addPlate(plate5);
-	cube2.addPlate(plate6);
-
-	cube3.addPlate(plate1);
-	cube3.addPlate(plate2);
-	cube3.addPlate(plate3);
-	cube3.addPlate(plate4);
-	cube3.addPlate(plate5);
-	cube3.addPlate(plate6);
-
-	cube4.addPlate(plate1);
-	cube4.addPlate(plate2);
-	cube4.addPlate(plate3);
-	cube4.addPlate(plate4);
-	cube4.addPlate(plate5);
-	cube4.addPlate(plate6);
-
-	//camRotation.y=0.5;
 
 	cout << "TC3L Test" << endl;
-
-
-	cube.setPosition(position1);
-	cube2.setPosition(position2);
-	cube3.setPosition(position3);
-	cube4.setPosition(position4);
 
 	XSelectInput(di, wi, KeyPressMask | KeyReleaseMask | ExposureMask | PointerMotionMask);
 	XEvent ev;
@@ -133,11 +89,13 @@ int main()
 			camRotation.y = (960-x)/300 ;
 			camRotation.x = (y-540)/300 ;
 			double dump = 0 ;
-			XClearWindow(di, wi) ;
+			//XClearWindow(di, wi) ;
 			cube.render();
 			cube2.render();
 			cube3.render();
 			cube4.render();
+			cube5.render();
+			redrawBuf();
 		}
 		if (ev.type == KeyPress)
     {
@@ -158,12 +116,24 @@ int main()
 					camPosition.x += sin(camRotation.y)*10;
 					camPosition.z -= cos(camRotation.y)*10;
 				}
+				else if(ev.xkey.keycode == 38)
+				{
+					camPosition.x += sin(camRotation.y-1.5708)*10;
+					camPosition.z -= cos(camRotation.y-1.5708)*10;
+				}
+				else if(ev.xkey.keycode == 40)
+				{
+					camPosition.x += sin(camRotation.y+1.5708)*10;
+					camPosition.z -= cos(camRotation.y+1.5708)*10;
+				}
 
-				XClearWindow(di, wi) ;
+				//XClearWindow(di, wi) ;
 				cube.render();
 				cube2.render();
 				cube3.render();
 				cube4.render();
+				cube5.render();
+				redrawBuf();
     }
 	}
 	return 0;
